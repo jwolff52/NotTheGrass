fa6cc014fa63b80f74d6b00ef140cf7d2c0221e1
James Wolff <james.wolff0@gmail.com>
2024-02-18 17:16:10 -0600

    Remove default config settings and add our own

--- src/main/java/me/jameswolff/notthegrass/Config.java
+++ src/main/java/me/jameswolff/notthegrass/Config.java
@@ -1,16 +1,16 @@
 package me.jameswolff.notthegrass;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
@@ -19,45 +19,48 @@ public class Config
 {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
+    private static final ModConfigSpec.ConfigValue<List<? extends String>> TAG_LIST = BUILDER
+        .comment("List of block tags that should be processed by the mod.")
+        .defineListAllowEmpty("tagList", List.of("minecraft:sword_efficient"), Config::validateTagKey);
 
-    private static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
-
-    public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-            .comment("What you want the introduction message to be for the magic number")
-            .define("magicNumberIntroduction", "The magic number is... ");
-
-    // a list of strings that are treated as resource locations for items
-    private static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-            .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+    private static final ModConfigSpec.ConfigValue<List<? extends String>> BLOCK_IGNORE_LIST = BUILDER
+        .comment("List of blocks to always process as normal. Even if they have a tag in the tag list.")
+        .defineListAllowEmpty("blockIgnoreList", List.of("minecraft:carved_pumpkin"), Config::validateBlockName);
 
     static final ModConfigSpec SPEC = BUILDER.build();
 
-    public static boolean logDirtBlock;
-    public static int magicNumber;
-    public static String magicNumberIntroduction;
-    public static Set<Item> items;
+    public static Set<ResourceLocation> tagList;
+    public static Set<Block> blockIgnoreList;
 
-    private static boolean validateItemName(final Object obj)
+    private static boolean validateTagKey(final Object obj)
     {
-        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(new ResourceLocation(itemName));
+        return obj instanceof String tagKey && BuiltInRegistries.BLOCK.getTags().anyMatch(tag -> tag.toString().equals(tagKey));
     }
 
+    private static boolean validateBlockName(final Object obj)
+    {
+        return obj instanceof String blockName && BuiltInRegistries.BLOCK.containsKey(new ResourceLocation(blockName));
+    }
+
+    // This method is called when the config is loaded
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event)
     {
-        logDirtBlock = LOG_DIRT_BLOCK.get();
-        magicNumber = MAGIC_NUMBER.get();
-        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
+        NotTheGrass.LOGGER.info("Loading config");
 
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream()
-                .map(itemName -> BuiltInRegistries.ITEM.get(new ResourceLocation(itemName)))
-                .collect(Collectors.toSet());
+        // Load the config values into the fields
+        tagList = TAG_LIST.get().stream()
+            .map(tag -> new ResourceLocation(tag))
+            .collect(Collectors.toSet());
+        NotTheGrass.LOGGER.info("Tag list loaded:");
+        tagList.forEach(tag -> NotTheGrass.LOGGER.info(tag.toString()));
+
+        blockIgnoreList = BLOCK_IGNORE_LIST.get().stream()
+            .map(block -> BuiltInRegistries.BLOCK.get(new ResourceLocation(block)))
+            .collect(Collectors.toSet());
+        NotTheGrass.LOGGER.info("Block ignore list loaded:");
+        blockIgnoreList.forEach(block -> NotTheGrass.LOGGER.info(block.toString()));
+
+        NotTheGrass.LOGGER.info("Config loaded");
     }
 }
